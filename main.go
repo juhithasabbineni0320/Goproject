4366bcbbcf0605d3f4756f4cef5d87a9e065217b
Juhitha Sabbineni <juhitha.edu22@gmail.com>
2025-08-18 16:20:56 -0500

    add Address struct and NewPerson function

--- main.go
+++ main.go
@@ -2,39 +2,47 @@ package main
 
 import (
     "fmt"
-    "go-demo/utils" // Adjust module name or use relative import if needed
+    "go-demo/utils"
 )
 
 func main() {
-    // Basic data types
-    var integer int = 42
-    var floating float64 = 3.14
-    var boolean bool = true
-    var str string = "Hello, Go!"
+    // --- Structs with Pointers ---
+    person1 := utils.NewPerson("Julia", "8100 Main st", "Apt 4B", "Frisco", "Texas", 75034)
+    person2 := utils.NewPerson("Brick", "456 Oak Ave", "", "Austin", "Texas", 73301)
 
-    fmt.Println("Integer:", integer)
-    fmt.Println("Float:", floating)
-    fmt.Println("Boolean:", boolean)
-    fmt.Println("String:", str)
+    fmt.Println("Name:", person1.Name)
+    fmt.Println("Address1:", person1.Address.Address1)
+    fmt.Println("Address2:", person1.Address.Address2)
+    fmt.Println("City:", person1.Address.City)
+    fmt.Println("State:", person1.Address.State)
+    fmt.Println("Zip:", person1.Address.Zip)
 
-    // Using flow control from utils package
-    fmt.Println("\nChecking number flow control with 15:")
-    utils.CheckNumber(15)
+    fmt.Println()
 
-    fmt.Println("\nChecking number flow control with -5:")
-    utils.CheckNumber(-5)
+    fmt.Println("Name:", person2.Name)
+    fmt.Println("Address1:", person2.Address.Address1)
+    fmt.Println("Address2:", person2.Address.Address2)
+    fmt.Println("City:", person2.Address.City)
+    fmt.Println("State:", person2.Address.State)
+    fmt.Println("Zip:", person2.Address.Zip)
 
-    fmt.Println("\nLoop example with n=5:")
-    utils.LoopExample(5)
+    // --- Arrays ---
+    fmt.Println("\n--- Arrays ---")
+    var numbers [5]int = [5]int{10, 20, 30, 40, 50}
+    fmt.Println("Array:", numbers)
 
-    // Demonstrate simple for loop in main
-    fmt.Println("\nSimple for loop in main:")
-    for i := 1; i <= 3; i++ {
-        fmt.Printf("Iteration %d\n", i)
+    // --- Slices ---
+    fmt.Println("\n--- Slices Example ---")
+    fruits := []string{"Apple", "Banana", "Mango"}
+    fmt.Println("Slice:", fruits)
+
+    // Append new elements
+    fruits = append(fruits, "Orange", "Grapes")
+    fmt.Println("After Append:", fruits)
+
+    // Iterate over slice
+    fmt.Println("\nIterating over fruits slice:")
+    for i, fruit := range fruits {
+        fmt.Println( i, fruit)
     }
-
-    // Defer example directly in main
-    defer fmt.Println("Deferred statement: This prints at the end of main")
-
-    fmt.Println("Main function is about to end")
 }
--- utils/utils.go
+++ utils/utils.go
@@ -1,36 +1,30 @@
 package utils
 
-import "fmt"
 
-// CheckNumber demonstrates if-else and switch with flow control
-func CheckNumber(num int) {
-    if num < 0 {
-        fmt.Println("Number is negative")
-    } else if num == 0 {
-        fmt.Println("Number is zero")
-    } else {
-        fmt.Println("Number is positive")
-    }
-
-    switch {
-    case num < 10:
-        fmt.Println("Number is less than 10")
-    case num >= 10 && num < 100:
-        fmt.Println("Number is between 10 and 99")
-    default:
-        fmt.Println("Number is 100 or more")
-    }
+type Address struct {
+    Address1 string
+    Address2 string
+    City     string
+    State    string
+    Zip      int
 }
 
-// LoopExample demonstrates for loops and defer
-func LoopExample(n int) {
-    fmt.Println("For loop from 0 to", n-1)
-    for i := 0; i < n; i++ {
-        fmt.Println(i)
-    }
+type Person struct {
+    Name    string
+    Address *Address // using pointer to Address struct
+}
 
-    fmt.Println("Using defer to print numbers in reverse:")
-    for i := 0; i < n; i++ {
-        defer fmt.Println(i)
+// Function to create a new Person
+func NewPerson(name string, addr1, addr2, city, state string, zip int) *Person {
+    addr := &Address{
+        Address1: addr1,
+        Address2: addr2,
+        City:     city,
+        State:    state,
+        Zip:      zip,
+    }
+    return &Person{
+        Name:    name,
+        Address: addr,
     }
 }
